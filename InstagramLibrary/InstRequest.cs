5f138672bdf4e96c8aa2c5fd409d3b4d0d205b13
sitskomickhail <micklewillwill911@gmail.com>
2019-06-12 15:58:05 +0300

    UI updating

--- InstagramLibrary/InstRequest.cs
+++ InstagramLibrary/InstRequest.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
